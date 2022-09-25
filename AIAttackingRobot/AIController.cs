a5b5181154db5fbc73a4defe6b1ecbe4a70b656a
SuryaHlwasia <47364015+SuryaHlwasia@users.noreply.github.com>
2022-09-25 16:37:04 +0530

    Add files via upload

---
+++ AIAttackingRobot/AIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour {
+
+	public Transform player;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		anim.SetFloat("distance", Vector3.Distance(transform.position,player.transform.position));	
+		anim.SetFloat("visangle", Vector3.Angle(this.transform.forward,player.transform.position - transform.position));	
+	}
+}
